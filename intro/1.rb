7fc2349e35595f1cffb3e399c32811642b8bbff8
Shoumik Jamil <sjamil@berkeley.edu>
2021-06-28 21:21:26 -0700

    Intro lesson

---
+++ intro/1.rb
@@ -0,0 +1,44 @@
+my_num = 25
+
+puts my_num
+
+# Arithmetic
+
+puts 5 / 7
+
+puts 5 ** 2
+
+puts 25 % 7
+
+puts "Hello world!"
+
+# String methods
+
+puts "myname".length
+
+puts "myname".reverse
+
+puts "myname".upcase
+
+puts "myname".downcase
+
+=begin
+Testing multiline comments
+Another line
+=end
+
+sum = 13 + 379
+
+product = 923 * 15
+
+quotient = 13209 / 17
+
+puts sum
+
+puts product 
+
+puts quotient
+
+name = "myname"
+
+puts name.downcase.reverse.upcase
